ba07bfb70b8446806cdd099c40b04e9b720c958d
Hunter Gibbs <hunter-g@iherb.com>
2017-10-23 11:49:59 -0700

    syntax error line 16

--- iHerb.Checkout.EditCart/index.js
+++ iHerb.Checkout.EditCart/index.js
@@ -11,7 +11,7 @@ http.createServer(function(req, res) {
 
     var html = ReactDOMServer.renderToStaticMarkup(body(null,
 
-      div({id: 'root', dangerouslySetInnerHTML: {__html:
+      div({id: 'root', dangerouslySetInnerHTML: {__html:''
         //ReactDOMServer.renderToString(App(props))
       }}),
 
