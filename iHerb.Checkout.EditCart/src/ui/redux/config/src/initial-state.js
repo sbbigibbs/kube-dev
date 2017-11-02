89bd31036d3bfebc433abafb2861660e27944605
Hunter Gibbs <hunter-g@iherb.com>
2017-11-01 17:24:15 -0700

    add language country currency

--- iHerb.Checkout.EditCart/src/ui/redux/config/src/initial-state.js
+++ iHerb.Checkout.EditCart/src/ui/redux/config/src/initial-state.js
@@ -3,5 +3,7 @@ export default fromJS({
     checkoutApi: "https://checkout-api.iherb.biz/v1",
     myAccountApi: "https://myaccount-api.iherb.biz/v1",
     loginToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiNGQ2NjM2ODUtMDVkMy00NjFkLTliOTItNWNiNzAxOGYyNTBkIiwibmJmIjoxNTA3NjY5MjQ1LCJleHAiOjE1MDgyNzQwNDUsImlzcyI6IlVHQ0FwaSJ9.8OxO6jsvBqv453lK7rPn26_1vA6xi3o2865ruylNetY",
-    language: "zh-TW"
+    language: "en-US",
+    currency: "USD",
+    country: "US"
 })
